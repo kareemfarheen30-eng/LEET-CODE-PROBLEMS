d82892f4950b14c7c7f43fc4aa1a11299898017f
farheen fathima <kareemfarheen30@gmail.com>
2026-07-21 09:03:52 +0545

    Update day 1-9 palindrome.java

--- day 1-9 palindrome.java
+++ day 1-9 palindrome.java
@@ -1,15 +1,19 @@
-import java.util.*;
-class ValidParentheses {
-    public boolean isValid(String s) {
-        Stack<Character> st = new Stack<>();
-        for(char c : s.toCharArray()) {
-            if(c == '(' || c == '{' || c == '[') st.push(c);
-            else {
-                if(st.isEmpty()) return false;
-                char top = st.pop();
-                if((c == ')' && top!= '(') || (c == '}' && top!= '{') || (c == ']' && top!= '[')) return false;
-            }
+class Solution {
+    public boolean isPalindrome(int x) {
+        // Negative numbers and numbers ending with 0 (except 0) are not palindromes
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
         }
-        return st.isEmpty();
+
+        int reversedHalf = 0;
+
+        while (x > reversedHalf) {
+            reversedHalf = reversedHalf * 10 + x % 10;
+            x /= 10;
+        }
+
+        // For even digits: x == reversedHalf
+        // For odd digits: x == reversedHalf / 10
+        return x == reversedHalf || x == reversedHalf / 10;
     }
 }
