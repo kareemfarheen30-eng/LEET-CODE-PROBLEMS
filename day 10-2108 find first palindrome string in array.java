0be259f41b914842b5c1c06a459763a240b1eb49
farheen fathima <kareemfarheen30@gmail.com>
2026-07-24 09:41:54 +0545

    Create day 10-2108 find first palindrome string in array.java

---
+++ day 10-2108 find first palindrome string in array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String firstPalindrome(String[] words) {
+        for (String s : words) {
+            if (isPalindrome(s)) {
+                return s;
+            }
+        }
+        return "";
+    }
+    
+    private boolean isPalindrome(String s) {
+        int l = 0, r = s.length() - 1;
+        while (l < r) {
+            if (s.charAt(l++) != s.charAt(r--)) return false;
+        }
+        return true;
+    }
+}
